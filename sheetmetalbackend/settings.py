be70050b584a6a1c8f82f7bc97248fdcc6d2d210
Josh Gulledge <jgulledge19@gmail.com>
2023-05-10 17:11:49 -0600

    Fixed installed apps settings

--- sheetmetalbackend/settings.py
+++ sheetmetalbackend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "todos.apps.TodosConfig",
+    "backend.apps.BackendConfig",
     "rest_framework",
     "corsheaders"
 ]
